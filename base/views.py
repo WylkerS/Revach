5519dd3857843fad132d13f5c7a21b43275e4a98
WylkerS <wylkersilvacorp@gmail.com>
2026-02-08 17:22:26 -0300

    feat: criação das pages transactions e categories

--- base/views.py
+++ base/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'pages/home_page.html')
+
+def transactions_page(request):
+    return render(request, 'pages/transactions_page.html')
+
+def categories_page(request):
+    return render(request, 'pages/categories_page.html')
--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from base.views import home_page
+from base.views import home_page, transactions_page, categories_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('transactions/', transactions_page, name='transactions'),
+    path('categories/', categories_page, name='categories'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Adicionar Isto
